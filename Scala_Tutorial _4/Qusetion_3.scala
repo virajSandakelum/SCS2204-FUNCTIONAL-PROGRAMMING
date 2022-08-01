75897f62a9239c8224457980049426e02f93c98c
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2022-08-01 22:19:59 +0530

    did some changes

--- Scala_Tutorial _4/Qusetion_3.scala
+++ Scala_Tutorial _4/Qusetion_3.scala
@@ -10,7 +10,10 @@ object Qusetion_5 extends App {
       fMethod(name)
   }
 
-  print(formatNames("Benny")(toUpper(_)))
+  println(formatNames("Benny")(toUpper(_)))
+  println(formatNames("Niroshan".substring(0,2))(toUpper(_)) + formatNames("Niroshan".substring(2))(toLower(_)))
+  println(formatNames("Saman")(toLower(_)))
+  println(formatNames("Kumara".substring(0,1))(toUpper(_)) + formatNames("Kumara".substring(1,5))(toLower(_)) + formatNames("Kumara".substring(5))(toUpper(_)))
 
 }
 
